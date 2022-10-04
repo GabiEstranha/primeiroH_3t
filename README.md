3b1f42c9a28c66a3db2eaadd6f7b735b72643ea5
GabiEstranha <111915085+GabiEstranha@users.noreply.github.com>
2022-10-04 16:55:54 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# primeiroH_3t
+# Terceiro Trimestre
+
+## Identificação
+Jesus - Nr. 50
+
+## Conteúdo
+HTML, CSS e javaScript
---
+++ estudo.js
@@ -0,0 +1,33 @@
+//alert("");
+//console.log("");
+//document.write("");
+
+var tabuada = 8;
+
+function meChame(nome){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
+    document.write("Feita por " + nome);
+    
+}
+
+function escreva(){
+    for(var i = 1; i < 11; i++){
+        document.write(tabuada + " x " + i + " = "+(tabuada*i)+"<br>");
+    }
+}
+
+function quadrado(){
+    for(var i = 2; i < 101; i++){
+        document.write("O Quadrado de " + i + " é " + (i*i)+ "<br>")
+    }
+}
