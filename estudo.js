9cd71f039aadda030a251a71d6c678e79ccbd48d
GabiEstranha <111915085+GabiEstranha@users.noreply.github.com>
2022-10-11 16:51:44 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -30,6 +30,7 @@ function quadrado(){
     for(var i = 2; i < 101; i++){
         document.write("O Quadrado de " + i + " é " + (i*i)+ "<br>")
     }
+
 }
 function total(){
     let valor = document.getElementById("val").value;
@@ -37,8 +38,31 @@ function total(){
     let mes = document.getElementById("mes").value;
     let resultado = 0;
     for(let i = 1; i <= mes; i++){
-        resultado += valor * (1+(juros/100));
+        resultado = valor * (1+(juros/100));
+        valor = resultado;
     }
     
     document.write("O Resultado é " + resultado);
+}
+
+function soma(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultado").innerHTML = r;
+}
+function média(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = (Number(n1) + Number(n2) + Number(n3))/3;
+    document.getElementById("resultado").innerHTML = r;
+}
+function necessário(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = 180 - (Number(n1) + Number(n2) + Number(n3));
+    document.getElementById("resultado").innerHTML = r;
 }
